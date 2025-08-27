c20d790e6dc82e17235c36bca57e9543a54ac027
Sarah Graves <sjgraves142@gmail.com>
2025-08-26 21:57:49 -0500

    Use descriptive variable names

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,22 +4,22 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 # https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('./eva_data.json', 'r', encoding='ascii')
-data_t = open('./eva_data.csv','w', encoding='utf-8')
-g_file = './cumulative_eva_graph.png'
+input_file = open('./eva_data.json', 'r', encoding='ascii')
+output_file = open('./eva_data.csv','w', encoding='utf-8')
+graph_file = './cumulative_eva_graph.png'
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
 data=[]
 
 for i in range(374):
-    line=data_f.readline()
+    line=input_file.readline()
     print(line)
     data.append(json.loads(line[1:-1]))
 #data.pop(0)
 ## Comment out this bit if you don't want the spreadsheet
 
-w=csv.writer(data_t)
+w=csv.writer(output_file)
 
 time = []
 date =[]
@@ -57,5 +57,5 @@ plt.plot(date,t[1:], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
-plt.savefig(g_file)
+plt.savefig(graph_file)
 plt.show()
