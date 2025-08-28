40eac289c1e0e0883e288d1002d79c1a16f9cf3f
Sarah Graves <sjgraves142@gmail.com>
2025-08-28 11:01:21 -0500

    Add new functionality and testing to calculate crew size

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re # added this line
 
 def main(input_file, output_file, graph_file):
     print("--START--")
 
     eva_data = read_json_to_dataframe(input_file)
 
+    eva_data = add_crew_size_column(eva_data) # added this line
+
     write_dataframe_to_csv(eva_data, output_file)
 
     plot_cumulative_time_in_space(eva_data, graph_file)
 
     print("--END--")
+
 
 def read_json_to_dataframe(input_file):
     """
@@ -45,6 +49,7 @@ def write_dataframe_to_csv(df, output_file):
     """
     print(f'Saving to CSV file {output_file}')
     df.to_csv(output_file, index=False)
+
 
 def text_to_duration(duration):
     """
@@ -104,6 +109,39 @@ def plot_cumulative_time_in_space(df, graph_file):
     plt.savefig(graph_file)
     plt.show()
 
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
 
 if __name__ == "__main__":
 
