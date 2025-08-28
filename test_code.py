8ef4368e4fdb93d29f3f7919f4ca24caa3ac8c8a
Sarah Graves <sjgraves142@gmail.com>
2025-08-28 09:39:09 -0500

    Update test for float and irrational numbers

--- test_code.py
+++ test_code.py
@@ -2,10 +2,22 @@ from eva_data_analysis import text_to_duration
 
 def test_text_to_duration_integer():
     input_value = "10:00"
-    test_result = text_to_duration(input_value)
-    assert text_to_duration(input_value) == 10 # built in keywork to run the assumption that something returns a T/F value
-    print(f"text_to_duration('10:00') == 10?", {test_result})
+    # OPTION 1: the following lines let us visually inspect if the output is right by reading the print statement
+    #test_result = text_to_duration(input_value)
+    #print(f"text_to_duration('10:00') == 10?", {test_result})
+    # OPTION 2: assert is alternative way to force an error if not correct
+    assert text_to_duration(input_value) == 10 # built in keyword to run the assumption that something returns a T/F value
 
 
+def test_text_to_duration_float():
+    actual_value = text_to_duration("10:15")
+    #print(f"actual value should be {actual_value}")
+    assert actual_value == 10.25
 
-    test_text_to_duration_integer()
+def test_text_to_duration_irrational():
+    assert abs(text_to_duration("10:20") - 10.33333) < 1e-5 # way to get around evaluating irrational numbers
+    #assert text_to_duration("10:20") == 10 + 1/3 # alternate suggestion
+
+test_text_to_duration_integer()
+test_text_to_duration_float()
+text_text_to_duration_irrational()
